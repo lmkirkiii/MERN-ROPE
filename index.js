1de4864c25b43cf86cca605000c4a95c34e81535
Lawrence Kirk <lmkirkiii@gmail.com>
2018-08-14 17:21:49 -0400

    worked on lab all afternoon only got create and read

--- index.js
+++ index.js
@@ -12,7 +12,60 @@ app.use(express.static('client/build'))
 app.use(cors())
 app.set('port', process.env.PORT || 3001)
 app.use(parser.json())
-app.use(require("./routes.js"));
+
+app.get('/api/greeting-card',(req,res)=> {
+  GreetingCard.find()
+  .then((greetings)=>{
+    for(var i = greetings.length -1; i > 0; i--){
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = greetings[i];
+      greetings[i] = greetings[j];
+      greetings[j] = temp;
+      var greeter = greetings[i]
+      console.log(greeter)
+      res.json(greeter)
+ }
+})
+.catch((err) => {
+console.log(err)
+})
+})
+
+
+
+app.post('/api/greeting-card',(req,res)=>{
+  GreetingCard.create({})
+  .then((greetings)=>{
+    greetings.save()
+    console.log(greetings)
+    res.json(greetings)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+app.get('/api/greeting-card/:id',(req,res)=>{
+  GreetingCard.findById(req.params.id)
+  .then((greetings)=>{
+    res.json(greetings)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+})
+
+app.delete('/api/greeting-card/',(req,res)=>{
+  console.log(res)
+  GreetingCard.deleteMany({})
+  .then((greetings)=>{
+  console.log(greetings)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+})
+
 
 
 app.listen(app.get('port'), () => {
