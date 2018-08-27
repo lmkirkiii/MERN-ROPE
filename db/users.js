7c2005c4d1633240150cba0bb930de71f7816502
Lawrence Kirk <lmkirkiii@gmail.com>
2018-08-27 09:15:57 -0400

    greeting card is important because it has auth

---
+++ db/users.js
@@ -0,0 +1,11 @@
+const mongoose = require('./connection')
+
+const User = new mongoose.Schema({
+	email: String,
+	password: String
+},
+{ collection : 'Users' });  
+
+mongoose.model('User', User)
+
+module.exports = mongoose.model("User",User;
--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const parser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('./db/models.js')
 const GreetingCard = mongoose.model('GreetingCard')
+const passport = require('./config/passport')()
+const userController = require('./userController/users.js')
+
+
+
 
 const app = express()
 
-
-
+app.use('/users', userController)
+app.use(passport.initialize())
 app.use(express.static('client/build'))
 app.use(cors())
 app.set('port', process.env.PORT || 3001)
@@ -34,7 +39,7 @@ console.log(err)
 
 
 app.post('/api/greeting-card',(req,res)=>{
-  GreetingCard.create({})
+  GreetingCard.create(req.body)
   .then((greetings)=>{
     greetings.save()
     console.log(greetings)
@@ -45,6 +50,8 @@ app.post('/api/greeting-card',(req,res)=>{
   })
 })
 
+
+
 app.get('/api/greeting-card/:id',(req,res)=>{
   GreetingCard.findById(req.params.id)
   .then((greetings)=>{
@@ -53,6 +60,23 @@ app.get('/api/greeting-card/:id',(req,res)=>{
   .catch((err)=>{
     console.log(err)
   })
+})
+
+app.put('/api/greeting-card/',(req,res)=>{
+  GreetingCard.updateOne({to: req.body},
+    {
+        $set:{ "to": req.body, 
+        from: req.body,
+        message: req.body
+
+    }
+    }).then(updated =>{
+        console.log(updated)
+        res.json(updated)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
 })
 
 app.delete('/api/greeting-card/',(req,res)=>{
